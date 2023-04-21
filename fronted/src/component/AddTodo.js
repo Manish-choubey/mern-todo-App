409beda35e74ccfdbd71bef8fe2d65b01dd8796b
Manish-choubey <109017549+Manish-choubey@users.noreply.github.com>
2023-04-21 18:55:13 +0530

    update

--- fronted/src/component/AddTodo.js
+++ fronted/src/component/AddTodo.js
@@ -1,42 +1,90 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { addTodo } from "../redux/todoaction";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, deleteTodo, getTodos, updateTodo } from '../redux/todoaction';
 
-const AddTodo = ({ addTodo }) => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+const Todo = () => {
+  const [text, setText] = useState('');
+  const [status, setStatus] = useState('pending');
+  const [editing, setEditing] = useState(false);
+  const [id, setId] = useState('');
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const auth = useSelector((state) => state.auth);
 
-  const handleSubmit = e => {
+  useEffect(() => {
+    dispatch(getTodos());
+  }, [dispatch]);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const newTodo = { title, description };
-    addTodo(newTodo);
-    setTitle("");
-    setDescription("");
+    dispatch(addTodo({ text, status }, auth.token));
+    setText('');
+  };
+
+  const handleEdit = (todo) => {
+    setEditing(true);
+    setText(todo.text);
+    setStatus(todo.status);
+    setId(todo.id);
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    dispatch(updateTodo(id, { text, status }, auth.token));
+    setEditing(false);
+    setText('');
+    setStatus('pending');
+    setId('');
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id, auth.token));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-        />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title text-center mb-4">Todo List</h3>
+              <form onSubmit={editing ? handleUpdate : handleSubmit}>
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Add todo" value={text} onChange={(e) => setText(e.target.value)} required />
+                </div>
+                <div className="form-group">
+                  <select className="form-control" value={status} onChange={(e) => setStatus(e.target.value)}>
+                    <option value="pending">Pending</option>
+                    <option value="in progress">In Progress</option>
+                    <option value="done">Done</option>
+                    <option value="completed">Completed</option>
+                  </select>
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">{editing ? 'Update' : 'Add'}</button>
+              </form>
+              <hr />
+              <ul className="list-group">
+                {todos.map((todo) => (
+                  <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
+                    <span>{todo.text}</span>
+                    <div>
+                      {todo.status === 'pending' && <span className="badge badge-primary mr-2">{todo.status}</span>}
+                      {todo.status === 'in progress' && <span className="badge badge-warning mr-2">{todo.status}</span>}
+                      {todo.status === 'done' && <span className="badge badge-info mr-2">{todo.status}</span>}
+                      {todo.status === 'completed' && <span className="badge badge-success mr-2">{todo.status}</span>}
+                      <button type="button" className="btn btn-primary mr-2" onClick={() => handleEdit(todo)}>Edit</button>
+                      <button type="button" className="btn btn-danger" onClick={() => handleDelete(todo.id)}>Delete</button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <label htmlFor="description">Description</label>
-        <input
-          type="text"
-          id="description"
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-        />
       </div>
-      <button type="submit">Add Todo</button>
-    </form>
-  );
-};
+   
+)
+}
 
-export default connect(null, { addTodo })(AddTodo);
+export default Todo
